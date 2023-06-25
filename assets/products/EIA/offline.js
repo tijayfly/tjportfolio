d5c94b87fb79728910be8896b5ab41f7da3796ae
TiJayFLY <tj.mortimer@gmail.com>
2023-06-26 00:51:02 +0100

    elevator sim

---
+++ assets/products/EIA/offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1494024114,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/elevator-sheet0.png",
+		"images/bgmask-sheet0.png",
+		"images/fella-sheet0.png",
+		"images/stop-sheet0.png",
+		"images/stop-sheet1.png",
+		"media/elevator action music remake.m4a",
+		"media/elevator_action_music_remake.ogg",
+		"media/91926__corsica-s__ding.m4a",
+		"media/91926__corsica-s__ding.ogg",
+		"media/192277__lebcraftlp__click.m4a",
+		"media/192277__lebcraftlp__click.ogg",
+		"media/161230__lolamadeus__large-metal-lift-door-closing.m4a",
+		"media/161230__lolamadeus__large-metal-lift-door-closing.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
